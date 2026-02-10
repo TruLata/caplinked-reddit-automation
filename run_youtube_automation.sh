26a72391669547a94dc4baf8d413261a9d2e6d0e
TruLata <82048870+TruLata@users.noreply.github.com>
2026-02-10 23:32:55 +0100

    Delete run_youtube_automation.sh

--- run_youtube_automation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/252452.cloudwaysapps.com/bhdwepebka/public_html/caplinked_youtube_automation
-python3 content_pipeline.py
-python3 runway_generator.py
-python3 youtube_uploader.py
